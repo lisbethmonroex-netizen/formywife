9f9d0bd42a3343271ebe1c896c275bc52a1b6869
lisbethmonroex-netizen <lisbethmonroex@gmail.com>
2026-04-15 01:10:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
-const pages = [
-  {
-    title: "Chapter One",
-    text: "I prepared this just for you..."
-  },
-  {
-    title: "Memory 1",
-    text: "Do you remember the moment we first met?"
-  },
-  {
-    title: "Memory 2",
-    text: "As time passes, you became even more valuable."
-  },
-  {
-    title: "Final",
-    text: "I'm glad you exist ❤️"
-  }
+let pages = [
+  "pages/1.png",
+  "pages/2.png",
+  "pages/3.png",
+  "pages/4.png",
+  "pages/5.png",
+  "pages/6.png",
+  "pages/7.png",
+  "pages/8.png",
+  "pages/9.png",
+  "pages/10.png",
+  "pages/11.png",
+  "pages/12.png",
+  "pages/13.png",
+  "pages/14.png",
+  "pages/15.png",
+  "pages/16.png",
+  "pages/17.png",
+  "pages/18.png",
+  "pages/19.png",
+  "pages/20.png",
+  "pages/21.png",
+  "pages/22.png",
+  "pages/23.png",
+  "pages/24.png",
+  "pages/25.png"
 ];
 
-let current = 0;
+let i = 0;
+
+const intro = document.getElementById("intro");
+const book = document.getElementById("book");
+const img = document.getElementById("img");
+const frame = document.getElementById("frame");
+const music = document.getElementById("music");
+const click = document.getElementById("click");
+
+function start() {
+  intro.style.display = "none";
+  book.classList.remove("hidden");
+
+  music.play().catch(()=>{});
+  render();
+}
 
 function render() {
-  document.getElementById("title").innerText = pages[current].title;
-  document.getElementById("text").innerText = pages[current].text;
+  img.src = pages[i];
 }
 
-function nextPage() {
-  if (current < pages.length - 1) {
-    current++;
-    render();
+function animate(callback) {
+  frame.classList.remove("fade");
+  void frame.offsetWidth;
+  frame.classList.add("fade");
+
+  if (click) {
+    click.currentTime = 0;
+    click.play();
+  }
+
+  setTimeout(callback, 150);
+}
+
+function next() {
+  if (i < pages.length - 1) {
+    animate(() => {
+      i++;
+      render();
+    });
   }
 }
 
-function prevPage() {
-  if (current > 0) {
-    current--;
-    render();
+function prev() {
+  if (i > 0) {
+    animate(() => {
+      i--;
+      render();
+    });
   }
 }
 
-render();
+function toggleMusic() {
+  if (music.paused) music.play();
+  else music.pause();
+}
